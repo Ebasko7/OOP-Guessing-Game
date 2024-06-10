7c869beb78402877b91a88dc35ded02f600686e8
ERIC_BASKOVICH <eric.baskovich@gmail.com>
2024-06-10 09:59:46 -0600

    solution

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,19 @@
 class GuessingGame():
-    # Write your code here
-    pass
+    def __init__(self, answer):
+        self.answer = answer 
+        self.result = False 
+
+    def guess(self,user_guess):
+        if user_guess > self.answer:
+            self.result = False 
+            return "high"
+        elif user_guess < self.answer:
+            self.result = False
+            return "low"
+        elif user_guess == self.answer:
+            self.result = True
+            return "correct"
+    
+    def solved(self):
+        return self.result 
+    
